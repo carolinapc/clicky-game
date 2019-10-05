a8af28001da684222564f80af0eec5081cb9859c
Carolina Cavalcanti <carolinapc@gmail.com>
2019-10-05 18:11:54 -0400

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Memory Game</h1>
+        <h3>Click on an image to earn points, but you can't click on the same image more than once!</h3>
+      </div>
+    </div>
+  );
+}
+ 
+export default Header;
